b92a90c27e3b9d9d8a35e9ab611853bf43ac3b3f
Joelmk28 <joelmuhindok@gmail.com>
2026-05-27 16:45:21 +0100

    RedisConfig

---
+++ src/main/java/com/techie/springbootrediscache/confi/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.techie.springbootrediscache.confi;
+
+import com.techie.springbootrediscache.dto.ProductDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cashManager(RedisConnectionFactory connectionFactory){
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))//Durée de la donnée en cash
+                .disableCachingNullValues()//Pas de valeur null
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair.fromSerializer(
+                                new Jackson2JsonRedisSerializer<>(ProductDto.class)
+                        ));
+
+return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(redisCacheConfiguration)
+        .build();
+    }
+}
